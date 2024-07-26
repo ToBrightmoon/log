920ea490c226ae8f54cab4114348b1510c673455
chengzhao <1870035544@qq.com>
2024-07-26 20:00:20 +0800

    实现了一个基本的格式化器

---
+++ base/formatter_base.cpp
@@ -0,0 +1,31 @@
+#include <chrono>
+#include <sstream>
+#include <iomanip>
+
+#include "formatter_base.h"
+
+using namespace Log;
+using namespace Log::Base;
+
+MessageFormatterBase::MessageFormatterBase(Log::LogLevel level, const std::string & msg, const std::string & file, int line)
+:message_(msg),file_(file),line_(line)
+{
+    level_ = level;
+}
+
+std::string MessageFormatterBase::format()
+{
+    std::ostringstream oss;
+    auto now = std::chrono::system_clock::now();
+
+    std::time_t now_time = std::chrono::system_clock::to_time_t(now);
+
+    std::tm* now_tm = std::localtime(&now_time);
+
+    oss << "[" << std::put_time(now_tm, "%Y:%m:%d:%H:%M:%S") << "]"
+        << "[" << getLevelString(level_) << "] "
+        << "[" << file_ << ":" << line_ << "] "
+        << message_ << "\n";
+
+    return oss.str();
+}
---
+++ base/formatter_base.h
@@ -0,0 +1,32 @@
+
+#ifndef EASY_LOG_FORMATTER_BASE_H
+#define EASY_LOG_FORMATTER_BASE_H
+
+#include "i_message_formatter.h"
+
+namespace Log
+{
+    namespace Base
+    {
+        class MessageFormatterBase : public IMessageFormatter
+        {
+        public:
+
+            explicit MessageFormatterBase(LogLevel,const std::string&,const std::string&,int);
+
+            std::string format() override;
+
+        private:
+
+            LogLevel level_;
+
+            std::string message_;
+
+            std::string file_;
+
+            int line_;
+        };
+    }
+}
+
+#endif //EASY_LOG_FORMATTER_BASE_H
