fd8138057b91d5a011870bd11fd61baf23a09d97
chengzhao <1870035544@qq.com>
2024-07-26 19:40:29 +0800

    增加基本的LogLevel

---
+++ log/log_level.h
@@ -0,0 +1,20 @@
+//
+// Created by chengzhao on 2024/7/26.
+//
+
+#ifndef EASY_LOG_LOG_LEVEL_H
+#define EASY_LOG_LOG_LEVEL_H
+
+namespace Log
+{
+    enum class LogLevel
+    {
+        INFO = 1,
+        WARN = 2,
+        DEBUG = 3,
+        ERROR = 4,
+        FATAL = 5
+    };
+}
+
+#endif //EASY_LOG_LOG_LEVEL_H
